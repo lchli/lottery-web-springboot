7be676db69c2de5822e8278565eeed7f474e16b8
lichenghang <lichenghang@babytree-inc.com>
2018-06-27 00:33:51 +0800

    ip

--- src/main/java/com/lchli/lottery/util/Constants.java
+++ src/main/java/com/lchli/lottery/util/Constants.java
@@ -2,8 +2,8 @@ package com.lchli.lottery.util;
 
 public final class Constants {
 
-    public static final String IP="192.168.1.101";
-    public static final String PORT="8088";
+    public static final String IP="h.tomjamescn.win";
+    public static final String PORT="12301";
     public static final String HOST="http://"+IP+":"+PORT;
 
     public static final String APP_KEY="lch-minote-#@#";
